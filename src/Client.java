7307a3609421b0a621a0c73a14fe3bdfed1b7657
Uni <shaunleong@outlook.com>
2016-05-17 23:42:18 +0800

    Able to get server talking to java client

--- src/Client.java
+++ src/Client.java
@@ -1,40 +1,34 @@
 import java.net.*;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-
 import java.io.*;
 
-
-
-
-
 public class Client {
-	public static void main(String [] args)
-	   {
-	      String serverName = "127.0.0.1";
-	      int port = 2323;
-	      try
-	      {
-	         System.out.println("Connecting to " + serverName +
-			 " on port " + port);
-	         
-	         Socket client = new Socket(serverName, port);
-	         System.out.println("Just connected to " 
-			 + client.getRemoteSocketAddress());
-	         
-	         OutputStream outToServer = client.getOutputStream();
-	         DataOutputStream out = new DataOutputStream(outToServer);
-	         out.writeUTF("Hello from haha "
-	                      + client.getLocalSocketAddress());
-	         
-	         InputStream inFromServer = client.getInputStream();
-	         DataInputStream in = new DataInputStream(inFromServer);
-	         System.out.println("Server says " + in.readUTF());
-	         client.close();
-	      }catch(IOException e)
-	      {
-	         e.printStackTrace();
-	      }
-	   }
+	public static void main(String[] args) {
+		String serverName = "127.0.0.1";
+		int port = 2323;
+		try {
+			System.out.println("Connecting to " + serverName + " on port " + port);
+
+			Socket client = new Socket(serverName, port);
+			System.out.println("Just connected to " + client.getRemoteSocketAddress());
+
+			/*read in response*/
+			PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())));
+
+			out.println("Hello from haha " + client.getLocalSocketAddress() + "\n");
+			out.flush();
+
+			/* read response */
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+			System.out.println("Server says: " + in.readLine());
+
+			in.close();
+			out.close();
+
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
