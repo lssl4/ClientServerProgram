621e333cad3262d4acacc0323f5e2c7d929ae689
Alex Miller <noton96@gmail.com>
2016-05-17 19:59:07 +0800

    fixed some stuff

--- server.c
+++ server.c
@@ -17,17 +17,12 @@ int main(int argc, char *argv[])
     struct sockaddr_in dest; /* socket info about the machine connecting to us */
     struct sockaddr_in serv; /* socket info about our server */
     int mysocket;            /* socket used to listen for incoming connections */
-    socklen_t socksize = sizeof(struct sockaddr_in);
-char ip4[INET_ADDRSTRLEN]; 
+    socklen_t socksize = sizeof(struct sockaddr_in); 
 
     memset(&serv, 0, sizeof(serv));           /* zero the struct before filling the fields */
     serv.sin_family = AF_INET;                /* set the type of connection to TCP/IP */
-    serv.sin_addr.s_addr = inet_addr(ADDR); /* set our address to any interface */
-    serv.sin_port = htons(PORTNUM);           /* set the server port number */
-
-
-inet_ntop(AF_INET, &(serv.sin_addr),ip4,INET_ADDRSTRLEN);
-printf("%s", ip4);    
+    serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
+    serv.sin_port = PORTNUM;           /* set the server port number */
 
     mysocket = socket(AF_INET, SOCK_STREAM, 0);
     // bind serv information to mysocket
@@ -35,15 +30,15 @@ printf("%s", ip4);
 
 
     // start listening, allowing a queue of up to 1 pending connection
-    listen(mysocket, 1);
-    int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+    listen(mysocket, 5);
+    
   
-    while(consocket)
+    while(1)
     {
+		int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
         printf("Incoming connection from %s - sending welcome\n", inet_ntoa(dest.sin_addr));
         send(consocket, msg, strlen(msg), 0); 
         close(consocket);
-        consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
     }
 
     close(mysocket);
