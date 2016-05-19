17a08a40661e092aa79f7d148ddb9f058a68245d
20753794 <20753794@ecm-csse2109-l.uniwa.uwa.edu.au>
2016-05-19 16:29:44 +0800

    Started Java SSL

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,5 @@
 import java.net.*;
-
+import javax.net.ssl.*;
 import java.io.*;
 
 public class Client {
@@ -8,17 +8,39 @@ public class Client {
 		int port = 2323;
 
 		BufferedReader in;
-		BufferedWriter out;
 
 		try {
+			
+			
+			
+			 SSLSocketFactory factory =
+						(SSLSocketFactory)SSLSocketFactory.getDefault();
+					    SSLSocket client =
+						(SSLSocket)factory.createSocket("130.95.252.73", 2323);
+
 			System.out.println("Connecting to " + serverName + " on port " + port);
 
-			Socket client = new Socket(serverName, port);
 			System.out.println("Just connected to " + client.getRemoteSocketAddress());
 
 			// Get output stream
-			out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-			out.write("Hello from " + client.getLocalSocketAddress());
+		    client.startHandshake();
+
+		    PrintWriter out = new PrintWriter(
+					  new BufferedWriter(
+					  new OutputStreamWriter(
+	     				  client.getOutputStream())));
+
+		    out.println("GET / HTTP/1.0");
+		    out.println();
+		    out.flush();
+		    
+
+		    /*
+		     * Make sure there were no surprises
+		     */
+		    if (out.checkError())
+			System.out.println(
+			    "SSLSocketClient:  java.io.PrintWriter error");
 
 			/* read response */
 			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -28,7 +50,7 @@ public class Client {
 			System.out.print("\n");
 
 			in.close();
-
+			out.close();
 			client.close();
 
 		} catch (IOException e) {
