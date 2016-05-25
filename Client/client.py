1f69864a653f5a49fc8bf4a1494fb6d1a3e29208
21512714 <21512714@ecm-csse2101-l.uniwa.uwa.edu.au>
2016-05-25 10:26:49 +0800

    Made python ssl work with py server

--- Client/client.py
+++ Client/client.py
@@ -1,11 +1,8 @@
 import socket
 import ssl
 #https://carlo-hamalainen.net/blog/2013/1/24/python-ssl-socket-echo-test-with-self-signed-certificate
-sslSock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#sslSock = ssl.wrap_socket(s,ca_certs="serv.cert",cert_reqs=ssl.CERT_REQUIRED)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sslSock = ssl.wrap_socket(s,keyfile="client.key",certfile="client.crt",ca_certs="serv.crt",cert_reqs=ssl.CERT_REQUIRED)
 sslSock.connect(("127.0.0.1",2323))
 sslSock.send(bytes("NOOOOOOOOOOOO - Darth Vader",'utf-8'))
-sslSock.flush()
-msg = str(sslSock.recvmsg(1024))
-print(msg)
 sslSock.close()
